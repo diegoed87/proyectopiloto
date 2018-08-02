3f6894e842c4f40fea31cbfaba9d4f2db434c926
CARLOS JAVIER PITA AMAYA <javierpita17@hotmail.com>
2018-08-02 10:53:13 -0500

    Se sube proyecto Web para prueba piloto en su primera versión

---
+++ PruebaPilotoEJB/ejbModule/com/asesoftware/pruebapiloto/integracion/GestionCliente.java
@@ -0,0 +1,20 @@
+package com.asesoftware.pruebapiloto.integracion;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class GestionCliente
+ */
+@Stateless
+@LocalBean
+public class GestionCliente {
+
+    /**
+     * Default constructor. 
+     */
+    public GestionCliente() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
